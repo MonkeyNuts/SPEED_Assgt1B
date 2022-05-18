4ff5dde597cbeae5428f114f030d3e34542a8451
Don Strange <dj.strange15@gmail.com>
2022-05-18 16:54:40 +1200

    Added Moderator and Analyst Lists
    I added the two pages for Moderator and Analyst, which at this stage are just copies of the view all articles page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Home from "./pages/home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
+import ModeratorList from "./pages/moderator-list";
+import AnalystList from "./pages/analyst-list";
 import NotFoundPage from "./pages/404";
 import { Route, Routes, NavLink, BrowserRouter } from 'react-router-dom'
 
@@ -15,14 +17,18 @@ const App = () =>  {
             <li><NavLink  to = "/">Home</NavLink></li>
                 <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
                 <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+                <li><NavLink to = "/ModeratorList">Moderator Article Queue</NavLink></li>
+                <li><NavLink to = "/AnalystList">Analyst Article Queue</NavLink></li>
             </ul>
             
           <div className="content">
           <Routes>
             
             <Route path="/" element={<Home />}/>
-            <Route  path="/SEPractice" element={<SEPractice />}/>
-            <Route  path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route path="/SEPractice" element={<SEPractice />}/>
+            <Route path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route path="/ModeratorList" element={<ModeratorList />}/>
+            <Route path="/AnalystList" element={<AnalystList />}/>
             <Route path='*' element={<NotFoundPage  />} />
           </Routes>
           </div>
