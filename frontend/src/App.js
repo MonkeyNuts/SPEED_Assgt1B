e6d0e92b9ba24922d155eedbfd894fbac6a40077
Will Quinn <wrdquinn@gmail.com>
2022-05-18 16:19:19 +1200

    frontend with depenencies needed for project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+import { Route, Routes, NavLink, BrowserRouter } from 'react-router-dom'
+
+const App = () =>  {
+    return (
+        <BrowserRouter>
+        <div>
+          <h1>Software Practice Empirical Evidence Database (SPEED) </h1>
+            <ul className="header">
+            <li><NavLink  to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+            </ul>
+            
+          <div className="content">
+          <Routes>
+            
+            <Route path="/" element={<Home />}/>
+            <Route  path="/SEPractice" element={<SEPractice />}/>
+            <Route  path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route path='*' element={<NotFoundPage  />} />
+          </Routes>
+          </div>
+        </div>
+        </BrowserRouter>
+    );
 }
-
+ 
 export default App;
