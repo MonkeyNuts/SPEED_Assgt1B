37f166d3dacd5abdcc2266816b3cd71ebda632e1
Don Strange <51571494+DorisTheGod@users.noreply.github.com>
2022-06-01 13:17:06 +1200

    Added User Context.js and Formatted App.js
    I have begun adding the UserType Global variable via a context, and have reformatted the App.js file using the inbuilt formatter in the VS Code IDE.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Home from "./pages/home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
 import ModeratorList from "./pages/moderator-list";
 import AnalystList from "./pages/analyst-list";
 import EditPage from "./pages/editPage";
@@ -10,42 +10,43 @@ import NotFoundPage from "./pages/404";
 import { Route, Routes, NavLink, BrowserRouter } from 'react-router-dom'
 import logo from "./images/SPEEDLOGO2.png";
 
-const App = () =>  {
-    return (
-        <BrowserRouter>
-        <div>
-          <img className="logo" src={logo} />
-            <ul className="header">
-            <li><NavLink  to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-                <li><NavLink to = "/ModeratorList">Moderator Article Queue</NavLink></li>
-                <li><NavLink to = "/AnalystList">Analyst Article Queue</NavLink></li>
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <img className="logo" src={logo} />
 
-                <select className="userstate">
-                <option value="">Select User state</option>
-                <option value="user">User</option>
-                <option value="mod">Moderator</option>
-                <option value="ana">Analyst</option>
-                </select>
-            </ul>
-            
-          <div className="content">
+        <ul className="header">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
+          <li><NavLink to="/ModeratorList">Moderator Article Queue</NavLink></li>
+          <li><NavLink to="/AnalystList">Analyst Article Queue</NavLink></li>
+
+          <select className="userstate">
+            <option value="">Select User state</option>
+            <option value="user">User</option>
+            <option value="mod">Moderator</option>
+            <option value="ana">Analyst</option>
+          </select>
+        </ul>
+
+        <div className="content">
           <Routes>
-            
-            <Route path="/" element={<Home />}/>
-            <Route path="/SubmitArticle" element={<SubmitArticle />}/>
-            <Route path="/SEPractice" element={<SEPractice />}/>
-            <Route path="/ModeratorList" element={<ModeratorList />}/>
-            <Route path="/AnalystList" element={<AnalystList />}/>
+
+            <Route path="/" element={<Home />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/ModeratorList" element={<ModeratorList />} />
+            <Route path="/AnalystList" element={<AnalystList />} />
             <Route path="/EditPage/:id" element={<EditPage />}></Route>
-            <Route path='*' element={<NotFoundPage  />} />
+            <Route path='*' element={<NotFoundPage />} />
           </Routes>
-          </div>
         </div>
-        </BrowserRouter>
-    );
-    
+      </div>
+    </BrowserRouter>
+  );
+
 }
 
 export default App;
